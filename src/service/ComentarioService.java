865cbceb65abf6042c9fd7ab5d5f650aa9b3bb2a
Luis-Perf <61641654+Luis-Perf@users.noreply.github.com>
2020-05-01 20:26:34 -0300

    Add files via upload

--- src/service/ComentarioService.java
+++ src/service/ComentarioService.java
@@ -1,5 +1,33 @@
 package service;
 
+import model.Comentario;
+
+import java.util.ArrayList;
+
+import dao.ComentarioDAO;
+
+
 public class ComentarioService {
+	ComentarioDAO dao = new ComentarioDAO();
+	
+	public String criar(Comentario comentario) {
+		return dao.criar(comentario);
+	}
+	
+	public void atualizar(Comentario comentario){
+		dao.atualizar(comentario);
+	}
+	
+	public void excluir(Comentario comentario){
+		dao.excluir(comentario);
+	}
+	
+	public Comentario carregar(String id){
+		return dao.carregar(id);
+	}
+	
+	public ArrayList<Comentario> listComentario(String id) {
+		return dao.listComentario(id);
+	}
 
 }
