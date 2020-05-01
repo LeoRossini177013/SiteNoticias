6535356f5ba51ee1f9b8bdea9f960cf15089a683
Luis-Perf <61641654+Luis-Perf@users.noreply.github.com>
2020-05-01 20:27:04 -0300

    Add files via upload

--- src/dao/ComentarioDAO.java
+++ src/dao/ComentarioDAO.java
@@ -4,24 +4,25 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import model.Comentario;
 
 public class ComentarioDAO {
-	public int criar(Comentario comentario) {
-		String sqlInsert = "INSERT INTO comentario(nome, comentario, idNoticia) VALUES (?, ?)";
+	public String criar(Comentario comentario) {
+		String sqlInsert = "INSERT INTO comentario(idNoticia, nome, comentario) VALUES (?, ?, ?)";
 		// usando o try with resources do Java 7, que fecha o que abriu
 		try (Connection conn = ConnectionFactory.obtemConexao();
 				PreparedStatement stm = conn.prepareStatement(sqlInsert);) {
-			stm.setString(1, comentario.getNome());
-			stm.setString(2, comentario.getComentario());
-			stm.setInt(3, comentario.getIdNoticia());
+			stm.setString(1, comentario.getIdNoticia());
+			stm.setString(2, comentario.getNome());
+			stm.setString(3, comentario.getComentario());
 			stm.execute();
 			String sqlQuery = "SELECT LAST_INSERT_ID()";
 			try (PreparedStatement stm2 = conn.prepareStatement(sqlQuery);
 					ResultSet rs = stm2.executeQuery();) {
 				if (rs.next()) {
-					comentario.setIdComentario(rs.getInt(1));
+					comentario.setIdComentario(String.valueOf(rs.getString(1)));
 				}
 			} catch (SQLException e) {
 				e.printStackTrace();
@@ -33,47 +34,47 @@ public class ComentarioDAO {
 	}
 
 	public void atualizar(Comentario comentario) {
-		String sqlUpdate = "UPDATE noticia SET titulo=?, conteudo=? WHERE id=?";
+		String sqlUpdate = "UPDATE comentario SET nome=?, comentario=? WHERE idComentario=?";
 		// usando o try with resources do Java 7, que fecha o que abriu
 		try (Connection conn = ConnectionFactory.obtemConexao();
 				PreparedStatement stm = conn.prepareStatement(sqlUpdate);) {
 			stm.setString(1, comentario.getNome());
 			stm.setString(2, comentario.getComentario());
-			stm.setInt(4, comentario.getIdComentario());
+			stm.setString(3, comentario.getIdComentario());
 			stm.execute();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
-	public void excluir(int id) {
-		String sqlDelete = "DELETE FROM noticia WHERE id = ?";
+	public void excluir(Comentario comentario) {
+		String sqlDelete = "DELETE FROM comentario WHERE idComentario = ?";
 		// usando o try with resources do Java 7, que fecha o que abriu
 		try (Connection conn = ConnectionFactory.obtemConexao();
 				PreparedStatement stm = conn.prepareStatement(sqlDelete);) {
-			stm.setInt(1, id);
+			stm.setString(1, comentario.getIdComentario());
 			stm.execute();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
-	public Comentario carregar(int id) {
-		Comentario noticia= new Comentario();
-		noticia.setIdComentario(id);
-		String sqlSelect = "SELECT nome, fone, email FROM cliente WHERE cliente.id = ?";
+	public Comentario carregar(String id) {
+		Comentario comentario= new Comentario();
+		comentario.setIdComentario(id);
+		String sqlSelect = "SELECT nome, comentario FROM comentario WHERE idComentario = ?";
 		// usando o try with resources do Java 7, que fecha o que abriu
 		try (Connection conn = ConnectionFactory.obtemConexao();
 				PreparedStatement stm = conn.prepareStatement(sqlSelect);) {
-			stm.setInt(1, noticia.getIdComentario());
+			stm.setString(1, comentario.getIdComentario());
 			try (ResultSet rs = stm.executeQuery();) {
 				if (rs.next()) {
-					noticia.setNome(rs.getString("nome"));
-					noticia.setComentario(rs.getString("comentario"));
+					comentario.setNome(rs.getString("nome"));
+					comentario.setComentario(rs.getString("comentario"));
 				} else {
-					noticia.setIdComentario(-1);
-					noticia.setNome(null);
-					noticia.setComentario(null);
+					comentario.setIdComentario(null);
+					comentario.setNome(null);
+					comentario.setComentario(null);
 				}
 			} catch (SQLException e) {
 				e.printStackTrace();
@@ -81,7 +82,35 @@ public class ComentarioDAO {
 		} catch (SQLException e1) {
 			System.out.print(e1.getStackTrace());
 		}
-		return noticia;
+		return comentario;
+	}
+	
+	public ArrayList<Comentario> listComentario(String id) {
+		String sqlSelect = "SELECT * from comentario WHERE idNoticia = ?";
+		try (Connection conn = ConnectionFactory.obtemConexao();
+				PreparedStatement stm = conn.prepareStatement(sqlSelect);) {
+			stm.setString(1, id);
+			try (ResultSet rs = stm.executeQuery();) {
+				ArrayList<Comentario> listId = new ArrayList<Comentario>();
+				while (rs.next()) {
+					Comentario comentario = new Comentario();
+					comentario.setIdComentario(rs.getString("idComentario"));
+					comentario.setNome(rs.getString("nome"));
+					comentario.setComentario(rs.getString("comentario"));
+					comentario.setIdNoticia(rs.getString("idNoticia"));
+					listId.add(comentario);
+				} 
+				
+					return listId;
+			}
+
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+		
 	}
 
 }
